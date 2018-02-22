ae6151cbcc3f7457d774ebeed1a22d32b4801283
shiimaxx <yoshima@shiimaxx.com>
2018-02-22 13:10:42 +0900

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import Vue from "vue";
+import * as request from "superagent";
 
 let v = new Vue({
   el: "#app",
   data: {
-    entry: []
+    entries: []
   },
+  methods: {
+    fetchEntries: function() {
+      var url:string "https://qiita.com/api/v2/users/shiimaxx/items";
+      var res = request
+        .get(url)
+        .then(function(res) {
+          return res.body;
+        })
+    }
+  }
 })
